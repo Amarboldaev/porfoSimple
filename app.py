7c95b23c4fb4402d3be3fb7a0251878c8c81999d
Amarbold <Amarbold.ar@gmail.com>
2021-01-06 01:56:46 +0800

    for pythonanywhere

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, request, redirect
 from datetime import datetime
 import pandas as pd
-import flashy
 import random
 
 
+class Flashcard:
+    def __init__(self):
+        try:
+            self.data = pd.read_csv("./data/words_to_learn.csv")
+        except FileNotFoundError:
+            self.original_data = pd.read_csv("./data/japanese_words.csv")
+            self.to_learn = self.original_data.to_dict(orient="records")
+        else:
+            self.to_learn = self.data.to_dict(orient="records") 
+    
+    current_card = {}
+
+    def next_card(self):
+        self.current_card = random.choice(self.to_learn)
+        return self.current_card
+
+
 app = Flask(__name__)
+
+
+
 
 bgcolor = ["red","blue","silver","green","yellow"]
 
@@ -20,12 +39,12 @@ def main():
 @app.route('/flashy', methods=["POST", "GET"])
 def Flashy():
     if request.method == "POST":
-        print("POST orj irlee")
+        # print("POST orj irlee")
         return redirect("/flashy")
     else:
-        hello = flashy.Flashcard()
+        hello = Flashcard()
         crand_list = hello.next_card()
-        print(crand_list)
+        # print(crand_list)
         return render_template("./flashcard.html", title = "Japanese" , word = crand_list["Japanese"], ran_color=random.choice(bgcolor), en_word = crand_list["English"], en_title="English")
 
 
