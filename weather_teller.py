094cba9a3f6c1733e95be0af2b17d1b89277f4fa
Amarbold <Amarbold.ar@gmail.com>
2021-01-09 23:29:36 +0800

    weather fixed for pythonanywhere

--- weather_teller.py
+++ weather_teller.py
@@ -11,28 +11,28 @@ auth_token = "018e1aef7815393965908c9ef8681195"
 HOUR = 12
 LATITUDE = 47.886398
 LONGITUDE = 106.905746
-API_KEY = "9964a280622f3b53374b028916985d94"
-URL = "https://api.openweathermap.org/data/2.5/onecall"
+API_KEY = '9964a280622f3b53374b028916985d94'
+URL = 'https://api.openweathermap.org/data/2.5/onecall'
 # https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 # https://api.openweathermap.org/data/2.5/onecall?lat=47.886398&lon=106.905746&appid=9964a280622f3b53374b028916985d94
 parameters = {
-    "lat": LATITUDE,
-    "lon": LONGITUDE,
-    "appid": API_KEY
+    'lat': LATITUDE,
+    'lon': LONGITUDE,
+    'appid': API_KEY
 }
-response = requests.get(f"{URL}", params=parameters)
+response = requests.get(f'{URL}', params=parameters)
 response.raise_for_status()
 weather_data = response.json()
-weather_slice = weather_data["hourly"][:12]
+weather_slice = weather_data['hourly'][:12]
 
 
 # learn list slice [:12] [start:stop:step]
 will_rain = False
 
 for hour_data in weather_slice:
-    condition_code = hour_data["weather"][0]["id"]
+    condition_code = hour_data['weather'][0]['id']
     if condition_code > 700:
-        # print("Bring an Umbrella")
+        # print('Bring an Umbrella')
         will_rain = True
         
 if will_rain:
